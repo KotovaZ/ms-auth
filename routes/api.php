d2eb7e4458fb3bea124af2d0e49193658b806277
Basil Tatarnikov <tatvasily@gmail.com>
2023-05-29 12:21:55 +0800

    feat: роутинг

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Login;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/test', function (Request $request) {
-    return '';
-});
+Route::post('/login', [Login::class, 'getJWT']);
