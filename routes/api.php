05305732be406d012521b4eccbbf8b619815281d
Basil Tatarnikov <tatvasily@gmail.com>
2023-05-30 11:17:54 +0800

    feat: зарегистрированы маршруты API

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,12 @@
 <?php
 
 use App\Http\Controllers\Login;
-use Illuminate\Http\Request;
+use App\Http\Controllers\SessionLogin;
+use App\Http\Controllers\SessionRegister;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
 Route::post('/login', [Login::class, 'getJWT']);
+Route::post('/session', [SessionRegister::class, 'register']);
+Route::get('/session', [SessionRegister::class, 'get']);
+Route::post('/session/{id}', [SessionLogin::class, 'login']);
